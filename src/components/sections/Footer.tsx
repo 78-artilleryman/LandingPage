4403f1bd3bc7ac4dde2b1a125b2735f946c8f81e
78-artilleryman <ywy040150@gmail.com>
2025-05-12 15:02:10 +0000

    ✨ Feat: 구글 애널리틱스 연결
    feat: 구글 애널리틱스 연결

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,9 +1,26 @@
-import Link from "next/link";
+"use client";
 
-export const Footer = () => (
-  <footer className="flex items-center justify-center gap-10 bg-gray-900 py-10">
-    <Link href="#" className="text-sm font-semibold text-gray-100 underline">
-      Instagram
-    </Link>
-  </footer>
-);
+import Link from "next/link";
+import { event } from "@/lib/gtag";
+
+export const Footer = () => {
+  const handleInstagramClick = () => {
+    event({
+      action: "click-instagram",
+      category: "social",
+      label: "instagram",
+    });
+  };
+
+  return (
+    <footer className="flex items-center justify-center gap-10 bg-gray-900 py-10">
+      <Link
+        href="https://www.instagram.com/sarangkkun_official/"
+        target="_blank"
+        onClick={handleInstagramClick}
+        className="text-sm font-semibold text-gray-100 underline">
+        Instagram
+      </Link>
+    </footer>
+  );
+};
