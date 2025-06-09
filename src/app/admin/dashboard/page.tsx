a4aeebe67f6c08e0bca06c8f27425f9342a8e5d9
78-artilleryman <ywy040150@gmail.com>
2025-06-09 06:30:26 +0000

    ♻️ Refactor: 로그인 컴포넌트 분리
    refactor: 로그인 컴포넌트 분리

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,31 +1,17 @@
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { use, useEffect } from "react";
 import { Suspense } from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import { DataTable } from "@/components/dashboard/dataTable";
 import { fetchEmailList } from "@/services/fetchEmail";
+import { authOptions } from "@/lib/auth";
+import TableSkeleton from "@/components/dashboard/TableSkeleton";
 
 export default async function AdminDashboard() {
-  // const { data: session, status } = useSession();
-  // const router = useRouter();
+  const session = await getServerSession(authOptions);
 
-  // useEffect(() => {
-  //   if (status === "unauthenticated") {
-  //     router.push("/admin/login");
-  //   }
-  // }, [status, router]);
-
-  // if (status === "loading") {
-  //   return (
-  //     <div className="flex min-h-screen items-center justify-center">
-  //       <div className="text-2xl">로딩 중...</div>
-  //     </div>
-  //   );
-  // }
-
-  // if (!session?.user) {
-  //   return null;
-  // }
+  if (!session?.user) {
+    redirect("/admin/login");
+  }
 
   const data = fetchEmailList({ page: 1, limit: 10 });
 
@@ -38,7 +24,7 @@ export default async function AdminDashboard() {
             <div className="overflow-hidden bg-white shadow sm:rounded-lg">
               <div className="px-4 py-5 sm:p-6">
                 <h3 className="text-lg leading-6 font-medium text-gray-900">이메일 구독자 관리</h3>
-                <Suspense fallback={<div>Loading...</div>}>
+                <Suspense fallback={<TableSkeleton />}>
                   <DataTable subscriptPromise={data} />
                 </Suspense>
               </div>
